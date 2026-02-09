5724c344e9b23143bd7f85ac6bc4d51b50d79a48
galaxygamerman <mohdimadiqbal@gmail.com>
2026-02-09 11:49:37 +0530

    Changed the indentation from spaces to tabs

--- databasemanager.cpp
+++ databasemanager.cpp
@@ -4,74 +4,71 @@
 #include <QtSql>
 
 DatabaseManager::DatabaseManager(QObject *parent)
-    : QObject(parent)
+	: QObject(parent)
 {
-    myDB = QSqlDatabase::addDatabase("QSQLITE");
-    myDB.setDatabaseName("users.db");
+	myDB = QSqlDatabase::addDatabase("QSQLITE");
+	myDB.setDatabaseName("users.db");
 
-    if (!myDB.open()) {
-        qDebug() << "Error: connection with database failed";
-    } else {
-        QSqlQuery query;
-        query.exec("CREATE TABLE IF NOT EXISTS users("
-                   "user_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                   "username TEXT,"
-                   "password TEXT,"
-                   "fullname TEXT,"
-                   "email TEXT,"
-                   "phone TEXT,"
-                   "age INTEGER,"
-                   "gender TEXT,"
-                   "userrole TEXT)");
-    }
+	if (!myDB.open()) {
+		qDebug() << "Error: connection with database failed";
+	} else {
+		QSqlQuery query;
+		query.exec("CREATE TABLE IF NOT EXISTS users("
+				   "user_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+				   "username TEXT,"
+				   "password TEXT,"
+				   "fullname TEXT,"
+				   "email TEXT,"
+				   "phone TEXT,"
+				   "age INTEGER,"
+				   "gender TEXT,"
+				   "userrole TEXT)");
+	}
 }
 
 bool DatabaseManager::registerUser(const QString &username,
-                                   const QString &password,
-                                   const QString &fullname,
-                                   const QString &email,
-                                   const QString &phone,
-                                   const QString &age,
-                                   const QString &gender,
-                                   const QString &userrole)
+								   const QString &password,
+								   const QString &fullname,
+								   const QString &email,
+								   const QString &phone,
+								   const QString &age,
+								   const QString &gender,
+								   const QString &userrole)
 {
-    qDebug() << "In the backend: " << username
-             << ' ' << password
-             << ' ' << fullname
-             << ' ' << email
-             << ' ' << phone
-             << ' ' << age
-             << ' ' << gender
-             << ' ' << userrole;
-    QSqlQuery query;
-    query.prepare("INSERT INTO users (username, password, fullname, email, phone, age, gender, userrole)"
-                  "VALUES (:username, :password, :fullname, :email, :phone, :age, :gender, :userrole)");
-    query.bindValue(":username", username);
-    query.bindValue(":password", password);
-    query.bindValue(":fullname", fullname);
-    query.bindValue(":email", email);
-    query.bindValue(":phone", phone);
-    query.bindValue(":age", age);
-    query.bindValue(":gender", gender);
-    query.bindValue(":userrole", userrole);
+	qDebug() << "In the backend: " << username << ' ' << password << ' ' << fullname << ' ' << email
+			 << ' ' << phone << ' ' << age << ' ' << gender << ' ' << userrole;
+	QSqlQuery query;
+	query.prepare(
+		"INSERT INTO users (username, password, fullname, email, phone, age, gender, userrole)"
+		"VALUES (:username, :password, :fullname, :email, :phone, :age, :gender, :userrole)");
+	query.bindValue(":username", username);
+	query.bindValue(":password", password);
+	query.bindValue(":fullname", fullname);
+	query.bindValue(":email", email);
+	query.bindValue(":phone", phone);
+	query.bindValue(":age", age);
+	query.bindValue(":gender", gender);
+	query.bindValue(":userrole", userrole);
 
-    return query.exec();
+	return query.exec();
 }
 
-bool DatabaseManager::checkUser(const QString &username, const QString &password, const QString &userrole){
-    qDebug() << "In the backend: " << username
-             << ' ' << password
-             << ' ' << userrole;
-    QSqlQuery query;
-    query.prepare("SELECT username, password FROM users WHERE username = :username AND password = :password");
-    query.bindValue(":username",username);
-    query.bindValue(":password",password);
+bool DatabaseManager::checkUser(const QString &username,
+								const QString &password,
+								const QString &userrole)
+{
+	qDebug() << "In the backend: " << username << ' ' << password << ' ' << userrole;
+	QSqlQuery query;
+	query.prepare(
+		"SELECT username, password FROM users WHERE username = :username AND password = :password");
+	query.bindValue(":username", username);
+	query.bindValue(":password", password);
 
-    if (!query.exec()){
-        qDebug() << "User login failed: " << query.lastError().text();
-        // TODO: Upon failed login, show the modal with a "Failed to authenticate" message
-        return false;
-    }
+	if (!query.exec()) {
+		qDebug() << "User login failed: " << query.lastError().text();
+		// TODO: Upon failed login, show the modal with a "Failed to authenticate" message
+		return false;
+	}
 
-    return query.next();
+	return query.next();
 }
--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
+	QGuiApplication app(argc, argv);
 
-    qmlRegisterType<DatabaseManager>("com.company.database", 1, 0, "DatabaseManager");
+	qmlRegisterType<DatabaseManager>("com.company.database", 1, 0, "DatabaseManager");
 
-    QQmlApplicationEngine engine;
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.loadFromModule("loginAndRegistration", "Main");
+	QQmlApplicationEngine engine;
+	QObject::connect(
+		&engine,
+		&QQmlApplicationEngine::objectCreationFailed,
+		&app,
+		[]() { QCoreApplication::exit(-1); },
+		Qt::QueuedConnection);
+	engine.loadFromModule("loginAndRegistration", "Main");
 
-    return app.exec();
+	return app.exec();
 }
